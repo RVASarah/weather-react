94f3fffd4dac37dbebaa50e80a893db3ecef5e49
RVASarah <115119579+RVASarah@users.noreply.github.com>
2023-01-17 13:57:37 -0500

    Added api integration

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
         <div className="weather-app-wrapper">
           <div className="weather-app">
             <Search />
-            <Weather />
+            <Weather defaultCity="Paris" />
             <Forecast />
           </div>
           <Footer />
--- src/Weather.js
+++ src/Weather.js
@@ -1,62 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
-export default function Weather() {
-  let weatherData = {
-    city: "Richmond",
-    temperature: "44",
-    date: "Monday 9:00",
-    imgUrl:
-      "https://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png",
-    description: "cloudy",
-    humidity: "50",
-    wind: "11",
-  };
-  return (
-    <div className="Weather">
-      <div className="overview">
-        <h1 id="city">{weatherData.city}</h1>
-        <ul>
-          <li id="date">{weatherData.date}</li>
-          <li id="description">{weatherData.description}</li>
-        </ul>
-        <div className="row">
-          <div className="col-4">
-            <div className="d-flex weather-temperature">
-              <div>
-                <strong id="temperature">{weatherData.temperature}</strong>
-                <span className="units">
-                  <span
-                    id="fahrenheit-link"
-                    className="active"
-                  >
-                    °F
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      city: response.data.city,
+      date: "Monday 09:00",
+      humidity: response.data.temperature.humidity,
+      description: response.data.condition.description,
+      iconUrl:
+        "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png",
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="overview">
+          <h1 id="city">{weatherData.city}</h1>
+          <ul>
+            <li id="date">{weatherData.date}</li>
+            <li id="description">{weatherData.description}</li>
+          </ul>
+          <div className="row">
+            <div className="col-4">
+              <div className="d-flex weather-temperature">
+                <div>
+                  <strong id="temperature">
+                    {Math.round(weatherData.temperature)}
+                  </strong>
+                  <span className="units">
+                    <span
+                      id="fahrenheit-link"
+                      className="active"
+                    >
+                      °F
+                    </span>
+                    |<span id="celsius-link">°C</span>
                   </span>
-                  |<span id="celsius-link">°C</span>
-                </span>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-4">
-            <img
-              className="weather-icon"
-              src={weatherData.imgUrl}
-              alt=""
-              id="icon"
-            />
-          </div>
-          <div className="col-4">
-            <ul>
-              <li>
-                Humidity: <span id="humidity">{weatherData.humidity}</span>%
-              </li>
-              <li>
-                Wind: <span id="wind">{weatherData.wind}</span> mph
-              </li>
-            </ul>
+            <div className="col-4">
+              <img
+                className="weather-icon"
+                src={weatherData.iconUrl}
+                id="icon"
+              />
+            </div>
+            <div className="col-4">
+              <ul>
+                <li>
+                  Humidity: <span id="humidity">{weatherData.humidity}</span>%
+                </li>
+                <li>
+                  Wind: <span id="wind">{weatherData.wind}</span> mph
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "d4a256f3bb9402t22bd7345b86oaccce";
+
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
