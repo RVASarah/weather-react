547e74cefe032e4f0e7ca19c65ecf4f2e15904ab
RVASarah <115119579+RVASarah@users.noreply.github.com>
2023-01-20 18:30:38 -0500

    Commented out the props

--- src/Weather.js
+++ src/Weather.js
@@ -6,20 +6,17 @@ export default function Weather(props) {
   return (
     <div className="Weather">
       <div className="overview">
-        <h1 id="city">{props.data.city}</h1>
+        <h1 id="city">{props.defaultCity.city}</h1>
         <ul>
-          <li id="date">
-            <FormattedDate date={props.data.date} />
-          </li>
-          <li id="description">{props.data.description}</li>
+          <li id="date">{/*<FormattedDate date={props.data.date}*/} </li>
+          <li id="description">{/*{props.data.description} */}</li>
         </ul>
         <div className="row">
           <div className="col-4">
             <div className="d-flex weather-temperature">
               <div>
-                <strong id="temperature">
-                  {Math.round(props.data.temperature)}
-                </strong>
+                {/*<strong id="temperature">
+                  {Math.round(props.data.temperature)} </strong> */}
 
                 <span className="units">
                   <span
@@ -34,21 +31,29 @@ export default function Weather(props) {
             </div>
           </div>
           <div className="col-4">
-            <img
+            {/*<img className="weather-icon">
               className="weather-icon"
               src={props.data.iconUrl}
               alt="icon"
               id="icon"
-            />
+                /> </img>*/}
           </div>
           <div className="col-4">
             <ul>
-              <li>
-                Humidity: <span id="humidity">{props.data.humidity}</span>%
+              {/*<li>
+                Humidity:{" "}
+                <span id="humidity">
+                  {props.data.humidity}
+                </span>
+                %
               </li>
               <li>
-                Wind: <span id="wind">{props.data.wind}</span> mph
-              </li>
+                Wind:{" "}
+                <span id="wind">
+                  {props.data.wind}
+                </span>{" "}
+                mph
+              </li> */}
             </ul>
           </div>
         </div>
